9123d23793593c5bfe5b063a22b33e74b419933b
IsacHolstad <wisac22@gmail.com>
2022-04-24 12:14:53 +0200

    fetching data from api

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 const myAPI = "http://rainydaysjackets.flywheelsites.com/wp-json/wc/store/products"
-console.log(myAPI)
+//console.log(myAPI)
 const divContainer = document.querySelector(".container");
 
 async function jackets(){
     try{
         const response = await fetch(myAPI);
-        console.log(response)
+        //console.log(response)
+        const responseJSON = await response.json()
+        const jcktData = responseJSON.data;
+        for (let i = 0; i < jcktData.length; i++) {
+            console.log(jcktData[i].name)
+           
+        }
+        divContainer.innerHTML += `<li>${jcktData[i].name}</li>`
     }
     catch(error){
         console.log("error")
