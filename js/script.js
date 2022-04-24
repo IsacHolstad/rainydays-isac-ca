3c8334bfd961862dcd43c27bfd6e9c39f5325fb4
IsacHolstad <wisac22@gmail.com>
2022-04-24 16:34:56 +0200

    fixed position

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const myAPI = "https://rainydaysjackets.flywheelsites.com/wp-json/wc/store/produ
 const proxy = "https://noroffcors.herokuapp.com/";
 const corsFixUrl = proxy + myAPI;
 //console.log(myAPI)
-const divContainer = document.querySelector(".container");
+const ulList = document.querySelector(".ul-list");
+const jacketImage = document.querySelector(".container")
 
 async function jackets() {
     try{
@@ -10,12 +11,12 @@ async function jackets() {
         console.log(response);
         const responseJSON = await response.json();
         console.log(responseJSON);
-        const jacketData = responseJSON.results;
+        const jacketData = responseJSON.data;
         //console.log(jacketData)
         for (let i = 0; i < responseJSON.length; i++) {
             console.log(responseJSON[i].name);
 
-            divContainer.innerHTML += `<li><span>${responseJSON[i].name}</span></li>`;
+            ulList.innerHTML += `<h1>${responseJSON[i].name}</h1>`;
 
         }
         
