80633346d0ec3d99e64443341bd01c96f4bde0ab
IsacHolstad <wisac22@gmail.com>
2022-04-24 15:08:13 +0200

    styled member login

--- js/script.js
+++ js/script.js
@@ -4,27 +4,7 @@ const corsFixUrl = proxy + myAPI;
 //console.log(myAPI)
 const divContainer = document.querySelector(".container");
 
-async function jackets(){
-    try{
-        const response = await fetch(corsFixUrl);
-        console.log(response)
-        const jsonData = await response.json()
-        console.log(jsonData)
-        const results = jsonData.data;
-        console.log(results)
-        for (let i = 0; i < jcktData.length; i++){
-            console.log(results[i].name)
-                divContainer.innerHTML += `<li>${results[i]}</li>`;
-           
-        }
-        
-    }
-    catch(error){
-        //console.log("error")
-        divContainer.innerHTML += `<h1>there is an error happening😓</h1>`
-    }
-}
-jackets();
+
 
 
 
