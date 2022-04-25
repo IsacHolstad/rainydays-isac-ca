41c105c42573cc087f81584af36291247311c8b7
IsacHolstad <wisac22@gmail.com>
2022-04-25 14:42:18 +0200

    card for jacket made

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,3 @@
 const jacketData = document.querySelector(".card")
-const API = "https://rainydaysjackets.flywheelsites.com/wp-json/wc/store/products/10"
+const API = "https://rainydaysjackets.flywheelsites.com/wp-json/wc/store/products/images/11"
 
