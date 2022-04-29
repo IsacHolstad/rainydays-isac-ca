0ba4d1137cc906d16d47ec376003a42ab5494be6
IsacHolstad <wisac22@gmail.com>
2022-04-29 19:49:29 +0200

    getting correct id in querystring

--- js/detail.js
+++ js/detail.js
@@ -4,30 +4,24 @@ const id = params.get('id')
 
 
 
-const ApiUrl = 'https://rainydaysjackets.flywheelsites.com/wp-json/wc/store/products/11'
+const ApiUrl = `https://rainydaysjackets.flywheelsites.com/wp-json/wc/store/products?category=11`
 console.log(ApiUrl)
-//const proxy = "https://noroffcors.herokuapp.com/";
-//const corsFixUrl = proxy + ApiUrl;
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFixUrl = proxy + ApiUrl;
 
 const jcktDataInfo = document.querySelector(".jacket");
 
-async function jcktInfo() {
+async function getJacket() {
     try{
-        const response = await fetch(ApiUrl);
-        //console.log(response);
-        const responseJSON = await response.json();
-        //console.log(responseJSON);
-        const jacketData = responseJSON.data;
-        console.log(responseJSON);
-        for (let i = 0; i < responseJSON.length; i++) {
-            console.log(responseJSON[i]);
+        const response = await fetch(corsFixUrl);
+        console.log(response);
+        const jacketData = await response.json();
+        //console.log(jacketData);
+        jcktDataInfo.innerHTML = `<li>${jacketData.id}</li>`
 
-            jcktDataInfo.innerHTML += `<img src="${responseJSON[i].images}"></li>`;
-        }
-        
     }
-    catch(e){
+    catch(error){
         console.log("error")
     }
 }
-jcktInfo()
+getJacket()
--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
-const myAPI = 'https://rainydaysjackets.flywheelsites.com/wp-json/wc/store/products';
-//const proxy = "https://noroffcors.herokuapp.com/";
-//const corsFixUrl = proxy + myAPI;
+const myAPI = "https://rainydaysjackets.flywheelsites.com/wp-json/wc/store/products";
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFixUrl = proxy + myAPI;
 //console.log(myAPI)
 const jacketPng = document.querySelector(".jacketpng");
 const jacketData = document.querySelector(".container")
+console.log(window.location)
 
 
 //console.log(window.location)
 
 async function showJackets() {
     try{
-        const response = await fetch(myAPI);
+        const response = await fetch(corsFixUrl);
         //console.log(response);
         const responseJSON = await response.json();
-        console.log(responseJSON);
-        const jacketInfo = responseJSON.data;
-        //console.log(responseJSON.data)
+        //console.log(responseJSON);
+        const jacketInfo = responseJSON.results;
+        //console.log(responseJSON)
         for (let i = 0; i < responseJSON.length; i++) {
-        //console.log(responseJSON[i].images);
+        console.log(responseJSON[i].id);
+        //console.log(responseJSON[i].name)
 
            
             jacketData.innerHTML += `<li class="list-details">
-            <a href="detaild-jckt.html?id=${responseJSON[i].price_html}",
-            <img src=${responseJSON[i].images.src}>,
-             ${responseJSON[i].name}</li>`
+            <a href="detaild-jckt.html?id=${responseJSON[i].id}">${responseJSON[i].name}</li>`
+            
 
             
         }
@@ -36,7 +37,7 @@ async function showJackets() {
 }
 showJackets();
 
-console.log(myAPI)
+//console.log(myAPI)
 
 
 
